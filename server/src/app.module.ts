b94d85527f33144b642e51b5c29a8dc02e833a6d
Yonatan Tizazu <yonatantizazu12@gmail.com>
2025-10-20 10:23:04 +0300

    feat: register prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './utils/auth'; // Your Better Auth instance
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule.forRoot({ auth }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
